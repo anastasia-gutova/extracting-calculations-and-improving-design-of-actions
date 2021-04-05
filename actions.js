d370700746a6ab523f1a457e81758329fa606da9
Anastasia Gutova <gutovanastasia@bk.ru>
2021-04-05 16:36:59 +0300

    выделение вычислений из действий

--- actions.js
+++ actions.js
@@ -1,120 +1,155 @@
-var isWarning = false;
 var tab1Array;
 
 function tableRowsProcessing() {
-  tab1Array = new Array(5);
-
-  tab1Array[0] = new Array(10);
-  tab1Array[1] = new Array(10);
-  tab1Array[2] = new Array(10);
-  tab1Array[3] = new Array(10);
-  tab1Array[4] = new Array(10);
-
+  tab1Array = cleanArray();
   $.each([1, 2, 3, 4, 5], function (rowIndex, num) {
     $("[id*='floatInput" + num + "']").each(function (column, el) {
       tab1Array[rowIndex][column] = parseFloat($(el).val());
-      $(el).click(function () {
-        if ($(this).val() == 0) {
-          $(this).val("");
-        }
-      });
-
-      $(el).change(function () {
-        if ($(this).val().indexOf('.') == 0 || $(this).val().length == 0) {
-          $(this).val("0" + $(this).val());
-          checkSumTimeRow(num - 1);
-        }
-      });
-      $(el).keyup(calculateFunction(rowIndex, column));
+      $(el).click(function () { tableOnElementClick(this) });
+      $(el).change(function () { tableOnElementChange(this, tab1Array, rowIndex) });
+      $(el).keyup(function () { tab1Array = updateTableCell(this, tab1Array, rowIndex, column) });
     });
     if (num != 3 && num != 5) {
-      checkSumTimeRow(num);
+      checkSumTimeRow(tab1Array, num);
     }
   });
 }
 
-function calculateFunction(rowIndex, column) {
-  tab1Array[rowIndex][column] = parseFloat($(this).val());
+function createArrayCopy(array) {
+  var copyArray = new Array(array.length);
+  for (var i = 0; i < array.length; i += 1) {
+    copyArray[i] = array[i].slice();
+  }
+  return copyArray
+}
+
+// Очищение массива значений
+function cleanArray() {
+  array = new Array(5);
+  array[0] = new Array(10);
+  array[1] = new Array(10);
+  array[2] = new Array(10);
+  array[3] = new Array(10);
+  array[4] = new Array(10);
+  return array
+}
+
+// Метод для очищения нулевого значения в input
+function tableOnElementClick(element) {
+  var currentElement = $(element);
+  currentElement.val() == 0 ? currentElement.val("") : '';
+}
+
+// Дописывает 0 для ситуации ввода числа ".5"
+function tableOnElementChange(element, array, rowIndex) {
+  var currentElement = $(element);
+  var currentElementValue = currentElement.val();
+  if (currentElementValue.indexOf('.') == 0 || currentElementValue.length == 0) {
+    writeZeroAtStart(currentElement);
+    checkSumTimeRow(array, rowIndex);
+  }
+}
+
+// Добавляет лидирующий 0 в инпут
+function writeZeroAtStart(element) {
+  element.val("0" + currentElementValue)
+}
+
+// Обновляет элементы таблицы
+function updateTableCell(element, array, rowIndex, column) {
+  var arrayCopy = createArrayCopy(array);
+  arrayCopy[rowIndex][column] = parseFloat($(element).val());
+  arrayCopy = updateConnectedCells(arrayCopy, rowIndex, column);
+
+  if (rowIndex == 0) {
+    checkSumTimeRow(arrayCopy, 1);
+    checkSumTimeRow(arrayCopy, 2);
+    checkSumTimeRow(arrayCopy, 4);
+  } else if (rowIndex == 2 || rowIndex == 4) {
+    checkSumTimeRow(arrayCopy, 2);
+    checkSumTimeRow(arrayCopy, 4);
+  }
+  return arrayCopy
+}
+
+function updateConnectedCells(array, rowIndex, column) {
+  var arrayCopy = createArrayCopy(array);
   if (rowIndex == 0 || rowIndex == 2 || rowIndex == 4) {
     var columnNum = (column + 1);
-    var newValue1 = tab1Array[0][column];
-    var newValue3 = tab1Array[2][column];
-    var newValue5 = tab1Array[4][column];
-    var element1 = isNaN(newValue1) ? 0 : newValue1;
-    var element3 = isNaN(newValue3) ? 0 : newValue3;
-    var element5 = isNaN(newValue5) ? 0 : newValue5;
-    var result2 = element1 / element3;
-    var result4 = (element1 / element3) - (element1 / element5);
-    var formattedResult2 = isFinite(result2) ? Math.round(result2) : 0;
-    var formattedResult4 = isFinite(result4) ? Math.round(result4) : 0;
+    var calculationResult = getNewCalculatedValues(arrayCopy[0][column], arrayCopy[2][column], arrayCopy[4][column]);
+    var resultRow2 = calculationResult[0];
+    var resultRow4 = calculationResult[1];
     var changedElement2 = $("[id='floatInput2_" + columnNum + "']");
     var changedElement4 = $("[id='floatInput4_" + columnNum + "']");
+    changedElement2.val(resultRow2);
+    changedElement4.val(resultRow4);
 
-    tab1Array[1][column] = formattedResult2;
-    tab1Array[3][column] = formattedResult4;
-    changedElement2.val(formattedResult2);
-    changedElement4.val(formattedResult4);
-    changeInputColor(changedElement2, formattedResult2);
-    changeInputColor(changedElement4, formattedResult4);
-    if (rowIndex == 0) {
-      checkSumTimeRow(1);
-      checkSumTimeRow(2);
-      checkSumTimeRow(4);
-    } else if (rowIndex == 2 || rowIndex == 4) {
-      checkSumTimeRow(2);
-      checkSumTimeRow(4);
-    }
+    arrayCopy[1][column] = resultRow2;
+    arrayCopy[3][column] = resultRow4;
+    changeInputColor(changedElement2, resultRow2);
+    changeInputColor(changedElement4, resultRow4);
   }
+  return arrayCopy
+}
+
+function getNewCalculatedValues(valueRow1, valueRow3, valueRow5) {
+  var notNanValue1 = isNaN(valueRow1) ? 0 : valueRow1;
+  var notNanValue3 = isNaN(valueRow3) ? 0 : valueRow3;
+  var notNanValue5 = isNaN(valueRow5) ? 0 : valueRow5;
+  var result2 = notNanValue1 / notNanValue3;
+  var result4 = result2 - (notNanValue1 / notNanValue5);
+
+  return [isFinite(result2) ? Math.round(result2) : 0, isFinite(result4) ? Math.round(result4) : 0]
 }
 
 function changeInputColor(el, value) {
-  if (value < 0) {
-    $(el).css('background-color', "#ffa4a4");
-    $(el).css('color', "#910000");
-    needWarning = true;
-  } else {
-    $(el).css('background-color', "");
-    $(el).css('color', "");
-  }
+  $(el).css('background-color', value < 0 ? "#ffa4a4" : "");
+  $(el).css('color', value < 0 ? "#910000" : "");
 }
 
 // Функция подсчета суммы в строке и сравнения с итогом
-function checkSumTimeRow(rowNum) {
-  sum = 0;
-  //Проходим по строке и суммируем все значения
-  for (element in tab1Array[rowNum - 1]) {
-    var value = isNaN(tab1Array[rowNum - 1][element]) ? 0 : tab1Array[rowNum - 1][element];
-    sum += value;
-  }
-
-  //Закрашиваем всю строку
+function checkSumTimeRow(array, rowNum) {
   var P1 = 0.001; // погрешность линейного пробега (1 строка)
   var P2 = 0.01; // погрешность для Времени локомотива в пути И показателя - время на промеж. станциях (2 и 4 строки)
 
   if (rowNum == 1) {
-    colorRowWithInaccuracy(P1, rowNum);
+    colorRowWithInaccuracy(array, P1, rowNum);
   } else if (rowNum == 2 || rowNum == 4) {
-    colorRowWithInaccuracy(P2, rowNum);
+    colorRowWithInaccuracy(array, P2, rowNum);
   }
 }
 
-function colorRowWithInaccuracy(inaccuracy, rowNum) {
-  var totalValue = parseFloat($("[id='totalRoad" + rowNum + " ']").text().replace(/ /g, '') == "" ? "0" : $("[id='totalRoad" + rowNum + " ']").text().replace(/ /g, ''));
+// Суммирование элеметов строки
+function getSumRow(array, rowNum) {
+  var sum = 0;
+  var arrayCopy = createArrayCopy(array);
+  //Проходим по строке и суммируем все значения
+  for (element in arrayCopy[rowNum - 1]) {
+    var value = isNaN(arrayCopy[rowNum - 1][element]) ? 0 : arrayCopy[rowNum - 1][element];
+    sum += value;
+  }
+  return sum
+}
+
+// Определение цвета окраски строк
+function colorRowWithInaccuracy(array, inaccuracy, rowNum) {
+  var sum = getSumRow(array, rowNum);
+  var totalRoadValue = $("[id='totalRoad" + rowNum + " ']").text().replace(/ /g, '');
+  var totalValue = parseFloat(totalRoadValue == "" ? "0" : totalRoadValue);
 
   if (!((totalValue * (1 + inaccuracy) >= sum) && (totalValue * (1 - inaccuracy) <= sum))) {
-    $("[id='row" + rowNum + " '] td").each(function (index, el) {
-      if (index != 12) {
-        $(el).css('background-color', "#ffa4a4");
-        $(el).css('color', "#910000")
-        needWarning = true;
-      }
-    });
+    colorAllRowCells(rowNum, "#ffa4a4", "#910000");
   } else {
-    $("[id='row" + rowNum + " '] td").each(function (index, el) {
-      if (index != 12) {
-        $(el).css("background-color", "#FFFFFF");
-        $(el).css("color", "#000000");
-      }
-    });
+    colorAllRowCells(rowNum, "#FFFFFF", "#000000");
   }
+}
+
+// Окраска строк
+function colorAllRowCells(rowNum, bgColor, textColor) {
+  $("[id='row" + rowNum + " '] td").each(function (index, el) {
+    if (index != 12) {
+      $(el).css('background-color', bgColor);
+      $(el).css('color', textColor);
+    }
+  });
 }
