c682e5992f9613ca95e0ca229ddf2a374efd0241
Anastasia Gutova <gutovanastasia@bk.ru>
2021-04-05 20:44:42 +0300

    Улучшение структуры действий

--- actions.js
+++ actions.js
@@ -1,26 +1,37 @@
 var tab1Array;
 
-function tableRowsProcessing() {
-  tab1Array = cleanArray();
-  $.each([1, 2, 3, 4, 5], function (rowIndex, num) {
-    $("[id*='floatInput" + num + "']").each(function (column, el) {
-      tab1Array[rowIndex][column] = parseFloat($(el).val());
-      $(el).click(function () { tableOnElementClick(this) });
-      $(el).change(function () { tableOnElementChange(this, tab1Array, rowIndex) });
-      $(el).keyup(function () { tab1Array = updateTableCell(this, tab1Array, rowIndex, column) });
-    });
-    if (num != 3 && num != 5) {
-      checkSumTimeRow(tab1Array, num);
-    }
-  });
-}
-
 function createArrayCopy(array) {
   var copyArray = new Array(array.length);
   for (var i = 0; i < array.length; i += 1) {
     copyArray[i] = array[i].slice();
   }
   return copyArray
+}
+
+function tableRowsProcessing() {
+  tab1Array = fillArray(cleanArray());
+  $.each([1, 2, 3, 4, 5], function (rowIndex, num) {
+    $("[id*='floatInput" + num + "']").each(function (column, el) { inputsActions(el, tab1Array, rowIndex, column) });
+    if (num != 3 && num != 5) {
+      checkSumTimeRow(tab1Array, num);
+    }
+  });
+}
+
+function inputsActions(el, array, rowIndex, column) {
+  $(el).click(function () { tableOnElementClick(this) });
+  $(el).change(function () { tableOnElementChange(this, array, rowIndex) });
+  $(el).keyup(function () { array = updateTableCell(this, array, rowIndex, column) });
+}
+
+function fillArray(array) {
+  var arrayCopy = createArrayCopy(array);
+  $.each([1, 2, 3, 4, 5], function (rowIndex, num) {
+    $("[id*='floatInput" + num + "']").each(function (column, el) {
+      arrayCopy[rowIndex][column] = parseFloat($(el).val());
+    });
+  });
+  return arrayCopy;
 }
 
 // Очищение массива значений
@@ -45,31 +56,27 @@ function tableOnElementChange(element, array, rowIndex) {
   var currentElement = $(element);
   var currentElementValue = currentElement.val();
   if (currentElementValue.indexOf('.') == 0 || currentElementValue.length == 0) {
-    writeZeroAtStart(currentElement);
+    currentElement.val("0" + currentElementValue);
     checkSumTimeRow(array, rowIndex);
   }
-}
-
-// Добавляет лидирующий 0 в инпут
-function writeZeroAtStart(element) {
-  element.val("0" + currentElementValue)
 }
 
 // Обновляет элементы таблицы
 function updateTableCell(element, array, rowIndex, column) {
   var arrayCopy = createArrayCopy(array);
   arrayCopy[rowIndex][column] = parseFloat($(element).val());
-  arrayCopy = updateConnectedCells(arrayCopy, rowIndex, column);
+  checkTableRow(arrayCopy, rowIndex);
+  return updateConnectedCells(arrayCopy, rowIndex, column);
+}
 
+function checkTableRow(array, rowIndex) {
   if (rowIndex == 0) {
-    checkSumTimeRow(arrayCopy, 1);
-    checkSumTimeRow(arrayCopy, 2);
-    checkSumTimeRow(arrayCopy, 4);
-  } else if (rowIndex == 2 || rowIndex == 4) {
-    checkSumTimeRow(arrayCopy, 2);
-    checkSumTimeRow(arrayCopy, 4);
+    checkSumTimeRow(array, 1);
   }
-  return arrayCopy
+  if (rowIndex == 0 || rowIndex == 2 || rowIndex == 4) {
+    checkSumTimeRow(array, 2);
+    checkSumTimeRow(array, 4);
+  }
 }
 
 function updateConnectedCells(array, rowIndex, column) {
@@ -86,8 +93,8 @@ function updateConnectedCells(array, rowIndex, column) {
 
     arrayCopy[1][column] = resultRow2;
     arrayCopy[3][column] = resultRow4;
-    changeInputColor(changedElement2, resultRow2);
-    changeInputColor(changedElement4, resultRow4);
+    updateInputColor(changedElement2, resultRow2);
+    updateInputColor(changedElement4, resultRow4);
   }
   return arrayCopy
 }
@@ -99,31 +106,35 @@ function getNewCalculatedValues(valueRow1, valueRow3, valueRow5) {
   var result2 = notNanValue1 / notNanValue3;
   var result4 = result2 - (notNanValue1 / notNanValue5);
 
-  return [isFinite(result2) ? Math.round(result2) : 0, isFinite(result4) ? Math.round(result4) : 0]
+  return [getNonFinite(result2), getNonFinite(result4)]
 }
 
-function changeInputColor(el, value) {
+// Возвращает конечное число
+function getNonFinite(result) {
+  return isFinite(result) ? Math.round(result) : 0;
+}
+
+function updateInputColor(el, value) {
   $(el).css('background-color', value < 0 ? "#ffa4a4" : "");
   $(el).css('color', value < 0 ? "#910000" : "");
 }
 
-// Функция подсчета суммы в строке и сравнения с итогом
 function checkSumTimeRow(array, rowNum) {
+  var sum = getSumRow(array, rowNum);
   var P1 = 0.001; // погрешность линейного пробега (1 строка)
   var P2 = 0.01; // погрешность для Времени локомотива в пути И показателя - время на промеж. станциях (2 и 4 строки)
 
   if (rowNum == 1) {
-    colorRowWithInaccuracy(array, P1, rowNum);
+    colorRowWithInaccuracy(sum, P1, rowNum);
   } else if (rowNum == 2 || rowNum == 4) {
-    colorRowWithInaccuracy(array, P2, rowNum);
+    colorRowWithInaccuracy(sum, P2, rowNum);
   }
 }
 
-// Суммирование элеметов строки
+// Суммирование элементов строки
 function getSumRow(array, rowNum) {
   var sum = 0;
   var arrayCopy = createArrayCopy(array);
-  //Проходим по строке и суммируем все значения
   for (element in arrayCopy[rowNum - 1]) {
     var value = isNaN(arrayCopy[rowNum - 1][element]) ? 0 : arrayCopy[rowNum - 1][element];
     sum += value;
@@ -131,13 +142,12 @@ function getSumRow(array, rowNum) {
   return sum
 }
 
-// Определение цвета окраски строк
-function colorRowWithInaccuracy(array, inaccuracy, rowNum) {
-  var sum = getSumRow(array, rowNum);
+// Определение цвета окраски строк с учетом погрешности
+function colorRowWithInaccuracy(rowSum, inaccuracy, rowNum) {
   var totalRoadValue = $("[id='totalRoad" + rowNum + " ']").text().replace(/ /g, '');
-  var totalValue = parseFloat(totalRoadValue == "" ? "0" : totalRoadValue);
+  var totalValueParsed = parseFloat(totalRoadValue == "" ? "0" : totalRoadValue);
 
-  if (!((totalValue * (1 + inaccuracy) >= sum) && (totalValue * (1 - inaccuracy) <= sum))) {
+  if (!((totalValueParsed * (1 + inaccuracy) >= rowSum) && (totalValueParsed * (1 - inaccuracy) <= rowSum))) {
     colorAllRowCells(rowNum, "#ffa4a4", "#910000");
   } else {
     colorAllRowCells(rowNum, "#FFFFFF", "#000000");
